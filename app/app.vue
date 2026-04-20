5f0607bcb990dfbce6c39937259f525e2d302c92
Mateusz Rüfenacht <dev.tadel@gmail.com>
2026-04-20 23:10:30 +0200

    glert :)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container">
+    <span class="title">glert</span>
   </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Libre+Barcode+39+Text&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #ede6dc;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.title {
+  font-family: 'Libre Barcode 39 Text';
+  font-size: 8rem;
+  color: #673137;
+}
+</style>
